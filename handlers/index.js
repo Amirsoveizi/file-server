3acd138c0d9a9c25e9f5e29139914ab3a335b5b0
a.soveizi <amirsoveizi2004@gmail.com>
2024-08-18 23:46:43 +0430

    move all handler functin to this directory

---
+++ handlers/index.js
@@ -0,0 +1,29 @@
+const {parse} = require('node:url')
+const {join} = require('node:path')
+const routes = require('../routes/routes')
+const errorHandler = require('../exception/handler');
+
+const allRoutes = routes()
+
+async function handler(request, response) {
+
+    const {
+        url,
+        method
+    } = request
+
+    const {
+        pathname
+    } = parse(url, true)
+
+    const key = method === 'POST' ? join(method,'::',pathname) : method
+    const route = allRoutes[key] || allRoutes.default
+
+    try {
+        await route(request, response);
+    } catch (error) {
+        await errorHandler(error,response);
+    }
+}
+
+module.exports = handler;
---
+++ handlers/routesHandler.js
@@ -0,0 +1,24 @@
+async function createFile(request,response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("this is create");
+}
+
+async function getFile(request,response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("this is get")
+}
+
+async function updateFile(request,response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("this is update")
+}
+
+async function deleteFile(request,response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("this is delete")
+}
+
+module.exports.createFile = createFile
+module.exports.getFile = getFile
+module.exports.updateFile = updateFile
+module.exports.deleteFile = deleteFile
